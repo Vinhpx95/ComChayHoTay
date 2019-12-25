6e111bce13c9f19dba6a9f3576fd94db4b45ff7b
Phạm Xuân Vinh <xuanvinh291195@gmail.com>
2019-12-25 19:48:22 +0700

    remove old source

--- storage/cms/cache/06/c4/header.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5de1cc57cd420575882138_7ae8da6cba1d7353eae32a193876bb04Class extends Cms\Classes\PartialCode
-{
-
-}
--- storage/cms/cache/a8/6f/default.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5de12f85accc5236497785_34c7bc51761cd726e3c6c62d0c32b7e7Class extends Cms\Classes\LayoutCode
-{
-
-}
